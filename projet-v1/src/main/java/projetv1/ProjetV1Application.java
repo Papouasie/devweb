a2a86f9f19f80dd0381802d18d00495045b90a6d
e1000139 <nedjma.benelhadj@gmail.com>
2015-09-26 09:27:32 +0200

    First commit

---
+++ projet-v1/src/main/java/projetv1/ProjetV1Application.java
@@ -0,0 +1,12 @@
+package projetv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetV1Application.class, args);
+    }
+}
